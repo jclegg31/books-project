92f43ef017f7ad53dc7eb9bd7547d83540f7da87
julie_c <forechecker@gmail.com>
2024-07-16 13:37:42 -0600

    pulled from localStorage

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 //get back the title of the book when the user enters the ISBN
 document.querySelector('button').addEventListener('click', getFetch);
 
+//on page load grabbing what's in books from localStorage and plulgs into dom
+document.querySelector('h3').textContent = localStorage.getItem('books');
+
 function getFetch() {
   const isbn = document.querySelector('input').value;
   console.log(isbn);
@@ -10,12 +13,19 @@ function getFetch() {
     .then((res) => res.json()) // parse response as JSON
     .then((data) => {
       console.log(data.title);
-      //put title into localStorage
-      localStorage.setItem('books', data.title);
+
+      if (!localStorage.getItem('books')) {
+        //if it's not null (falsy), will set to first book
+        localStorage.setItem('books', data.title);
+      } else {
+        //put title into localStorage
+        let books = localStorage.getItem('books') + ' ; ' + data.title;
+        localStorage.setItem('books', books);
+      }
 
       //document.querySelector('h3').textContent += data.title;
       //use localStorage instead
-      document.querySelector('h3').textContent += localStorage.getItem('books');
+      document.querySelector('h3').textContent = localStorage.getItem('books');
     })
     .catch((err) => {
       console.log(`error ${err}`);
